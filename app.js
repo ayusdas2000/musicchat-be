684dd188b3b677e6f5e020ab2e7becb445f53bb4
Ayus Das <40837456+ayusdas2000@users.noreply.github.com>
2022-07-26 21:03:02 +0530

    cors error corrected

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
-app.use(cors({
-  origin:"http://musicchat.herokuapp.com"
-}));
-
+app.use(
+  cors({
+    origin: "https://musicchat.herokuapp.com",
+    origin: "http://localhost:3000",
+  })
+);
 
 //Connect Database
 connectDB();
@@ -16,18 +18,18 @@ connectDB();
 app.use(express.json());
 
 // Define Routes
-app.use('/api/users', require('./routes/api/user'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
