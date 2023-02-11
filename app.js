28691a763726325698e1a4335e82c4031b9042bc
Ayus Das <40837456+ayusdas2000@users.noreply.github.com>
2023-02-11 17:32:27 +0530

    cors changed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 app.use(
   cors({
-    origin: "https://musicchat.herokuapp.com",
+    origin: "https://musicchat-wheat.vercel.app/",
   })
 );
 
