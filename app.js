2679945ac8f183970ca620a203d4b6a465667aaf
Ayus Das <40837456+ayusdas2000@users.noreply.github.com>
2023-02-12 13:55:39 +0530

    testing 2

--- app.js
+++ app.js
@@ -7,15 +7,7 @@ const app = express();
 // server.js
 
 //enables cors
-app.use(
-  cors({
-    allowedHeaders: ["sessionId", "Content-Type"],
-    exposedHeaders: ["sessionId"],
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-  })
-);
+app.use(cors());
 
 //Connect Database
 connectDB();
