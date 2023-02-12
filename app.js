467104ce7bbc6f73668fc24155328bcf6a1daf7f
Ayus Das <40837456+ayusdas2000@users.noreply.github.com>
2023-02-12 22:02:52 +0530

    slash removed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 // server.js
 
 //enables cors
-app.use(cors({ origin: "https://musicchat-wheat.vercel.app/" }));
+app.use(cors({ origin: "https://musicchat-wheat.vercel.app" }));
 
 //Connect Database
 connectDB();
